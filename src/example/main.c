56e99e122079f462004bcd1ef52f7654264a3619
Fabian Knorr <ers.trion@gmail.com>
2014-02-09 12:36:08 +0100

    Added Licensing Information

--- src/example/main.c
+++ src/example/main.c
@@ -1,8 +1,57 @@
+/** 
+ * Copyright (c) 2014, Fabian Knorr
+ * 
+ * This file is part of libgtk3-opengl.
+ *
+ * libgtk3-opengl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libgtk3-opengl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libgtk3-opengl. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This file is based on the work of André Diego Piske, 
+ * see <https://github.com/andrepiske/tegtkgl>. To retain André's licensing
+ * conditions on the parts of the software authored by him, the following 
+ * copyright notice shall be included in this and all derived files:
+ */
+
+/**
+ * Copyright (c) 2013 André Diego Piske
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <gtk/gtk.h>
-#include <gtk3-opengl.h>
+#include <gtkgl/canvas.h>
 #include <GL/gl.h>
 #include <math.h>
 
