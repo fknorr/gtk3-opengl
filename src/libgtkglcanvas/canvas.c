feb987fb3b05ae48f7094c345654aad0b4b2aa67
Fabian Knorr <ers.trion@gmail.com>
2015-03-29 12:37:55 +0200

    Removed call to deprecated gtk_widget_set_double_buffered

--- src/libgtkglcanvas/canvas.c
+++ src/libgtkglcanvas/canvas.c
@@ -194,7 +194,6 @@ gtk_gl_canvas_init(GtkGLCanvas *self) {
     gtk_widget_set_can_focus(wid, TRUE);
     gtk_widget_set_receives_default(wid, TRUE);
     gtk_widget_set_has_window(wid, TRUE);
-    gtk_widget_set_double_buffered(wid, FALSE);
 }
 
 
