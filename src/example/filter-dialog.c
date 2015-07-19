068ca8848892f4ec17f5045f818f5f3a21f30c66
Fabian Knorr <ers.trion@gmail.com>
2015-07-19 16:54:18 +0200

    Example filter dialog

---
+++ src/example/filter-dialog.c
@@ -0,0 +1,180 @@
+#include <gtk/gtk.h>
+#include <gtkgl/visual.h>
+#include <math.h>
+
+
+extern GtkDialog *example_filter_dialog;
+extern GtkGrid *example_filter_grid;
+extern GtkGLRequirement *example_requirements;
+
+
+typedef enum _AttrType {
+	ATTR_BOOL,
+	ATTR_INT,
+	ATTR_UNSIGNED,
+	ATTR_COLOR_TYPES,
+	ATTR_TRANSP_TYPE,
+	ATTR_CAVEAT
+} AttrType;
+
+
+typedef struct AttrDescriptor {
+	GtkGLAttribute attr;
+	const char *name;
+ 	AttrType type;
+} AttrDescriptor;
+
+
+static const AttrDescriptor attrs[] = {
+	{ GTK_GL_ACCELERATED, "Accelerated", ATTR_BOOL },
+	{ GTK_GL_COLOR_TYPES, "Color types", ATTR_COLOR_TYPES },
+	{ GTK_GL_COLOR_BPP, "Color BPP", ATTR_UNSIGNED },
+	{ GTK_GL_FB_LEVEL, "Framebuffer Level", ATTR_INT },
+	{ GTK_GL_DOUBLE_BUFFERED, "Double Buffered", ATTR_BOOL },
+	{ GTK_GL_STEREO_BUFFERED, "Stereo Buffered", ATTR_BOOL },
+	{ GTK_GL_AUX_BUFFERS, "Aux Buffers", ATTR_UNSIGNED },
+	{ GTK_GL_RED_COLOR_BPP, "Red Color BPP", ATTR_UNSIGNED },
+	{ GTK_GL_GREEN_COLOR_BPP, "Green Color BPP", ATTR_UNSIGNED },
+	{ GTK_GL_BLUE_COLOR_BPP, "Blue Color BPP", ATTR_UNSIGNED },
+	{ GTK_GL_ALPHA_COLOR_BPP, "Alpha Color BPP", ATTR_UNSIGNED },
+	{ GTK_GL_DEPTH_BPP, "Depth BPP", ATTR_UNSIGNED },
+	{ GTK_GL_STENCIL_BPP, "Stencil", ATTR_UNSIGNED },
+	{ GTK_GL_RED_ACCUM_BPP, "Red Accum. BPP", ATTR_UNSIGNED },
+	{ GTK_GL_GREEN_ACCUM_BPP, "Green Accum. BPP", ATTR_UNSIGNED },
+	{ GTK_GL_BLUE_ACCUM_BPP, "Blue Accum. BPP", ATTR_UNSIGNED },
+	{ GTK_GL_ALPHA_ACCUM_BPP, "Alpha Accum. BPP", ATTR_UNSIGNED },
+	{ GTK_GL_TRANSPARENT_TYPE, "Transparency Type", ATTR_TRANSP_TYPE },
+	{ GTK_GL_TRANSPARENT_INDEX_VALUE, "Transparent Index", ATTR_UNSIGNED },
+	{ GTK_GL_TRANSPARENT_RED, "Transparent Red", ATTR_UNSIGNED },
+	{ GTK_GL_TRANSPARENT_GREEN, "Transparent Green", ATTR_UNSIGNED },
+	{ GTK_GL_TRANSPARENT_BLUE, "Transparent Blue", ATTR_UNSIGNED },
+	{ GTK_GL_TRANSPARENT_ALPHA, "Transparent Alpha", ATTR_UNSIGNED },
+	{ GTK_GL_SAMPLE_BUFFERS, "Sample Buffers", ATTR_UNSIGNED },
+	{ GTK_GL_SAMPLES_PER_PIXEL, "Samples per Pixel", ATTR_UNSIGNED },
+	{ GTK_GL_CAVEAT, "Caveat", ATTR_CAVEAT }
+};
+
+
+enum {
+	ATTR_COUNT = sizeof(attrs)/sizeof(attrs[0])
+};
+
+
+void
+example_init_filter_dialog(void) {
+	size_t i;
+
+	for (i = 0; i < ATTR_COUNT; ++i) {
+		GtkLabel *name_label = GTK_LABEL(gtk_label_new(attrs[i].name));
+
+		GtkComboBoxText *req_box = GTK_COMBO_BOX_TEXT(gtk_combo_box_text_new());
+		gtk_combo_box_text_append_text(req_box, "Don't care");
+		gtk_combo_box_text_append_text(req_box, "Preferably equals");
+		gtk_combo_box_text_append_text(req_box, "Exactly equals");
+		gtk_combo_box_text_append_text(req_box, "At most");
+		gtk_combo_box_text_append_text(req_box, "At least");
+		gtk_combo_box_set_active(GTK_COMBO_BOX(req_box), 0);
+
+		GtkWidget *selector;
+		switch (attrs[i].type) {
+			case ATTR_BOOL: {
+				GtkComboBoxText *bool_box = GTK_COMBO_BOX_TEXT(
+						gtk_combo_box_text_new());
+				gtk_combo_box_text_append_text(bool_box, "False");
+				gtk_combo_box_text_append_text(bool_box, "True");
+				gtk_combo_box_set_active(GTK_COMBO_BOX(bool_box), 0);
+				selector = GTK_WIDGET(bool_box);
+				break;
+			}
+
+			case ATTR_INT: {
+				GtkAdjustment *adj = gtk_adjustment_new(0, -1000, 1000,
+						1, 1, 1);
+				GtkSpinButton *spin = GTK_SPIN_BUTTON(gtk_spin_button_new(
+						adj, 1, 0));
+				selector = GTK_WIDGET(spin);
+				break;
+			}
+
+			case ATTR_UNSIGNED: {
+				GtkAdjustment *adj = gtk_adjustment_new(0, 0, 1000,
+						1, 1, 1);
+				GtkSpinButton *spin = GTK_SPIN_BUTTON(gtk_spin_button_new(
+						adj, 1, 0));
+				selector = GTK_WIDGET(spin);
+				break;
+			}
+
+			case ATTR_COLOR_TYPES: {
+				GtkComboBoxText *color_box = GTK_COMBO_BOX_TEXT(
+						gtk_combo_box_text_new());
+				gtk_combo_box_text_append_text(color_box, "RGBA");
+				gtk_combo_box_text_append_text(color_box, "Indexed");
+				gtk_combo_box_text_append_text(color_box, "RGBA+Indexed");
+				gtk_combo_box_set_active(GTK_COMBO_BOX(color_box), 0);
+				selector = GTK_WIDGET(color_box);
+				break;
+			}
+
+			case ATTR_TRANSP_TYPE: {
+				GtkComboBoxText *transp_box = GTK_COMBO_BOX_TEXT(
+						gtk_combo_box_text_new());
+				gtk_combo_box_text_append_text(transp_box, "None");
+				gtk_combo_box_text_append_text(transp_box, "RGB");
+				gtk_combo_box_text_append_text(transp_box, "Index");
+				gtk_combo_box_set_active(GTK_COMBO_BOX(transp_box), 0);
+				selector = GTK_WIDGET(transp_box);
+				break;
+			}
+
+			case ATTR_CAVEAT: {
+				GtkComboBoxText *cave_box = GTK_COMBO_BOX_TEXT(
+						gtk_combo_box_text_new());
+				gtk_combo_box_text_append_text(cave_box, "None");
+				gtk_combo_box_text_append_text(cave_box, "Slow");
+				gtk_combo_box_text_append_text(cave_box, "Non-conformant");
+				gtk_combo_box_set_active(GTK_COMBO_BOX(cave_box), 0);
+				selector = GTK_WIDGET(cave_box);
+				break;
+			}
+		}
+
+		gtk_grid_attach(example_filter_grid, GTK_WIDGET(name_label), 0, i,
+				1, 1);
+		gtk_grid_attach(example_filter_grid, GTK_WIDGET(req_box), 1, i, 1, 1);
+		gtk_grid_attach(example_filter_grid, selector, 2, i, 1, 1);
+	}
+	gtk_widget_show_all(GTK_WIDGET(example_filter_grid));
+}
+
+
+void
+example_filter_dialog_run(void) {
+	size_t i, j=0;
+
+	gtk_dialog_run(example_filter_dialog);
+	gtk_widget_hide(GTK_WIDGET(example_filter_dialog));
+
+	g_free(example_requirements);
+	example_requirements = g_malloc0(ATTR_COUNT * sizeof(GtkGLRequirement));
+
+	for (i = 0; i < ATTR_COUNT; ++i) {
+		int combo_req = gtk_combo_box_get_active(GTK_COMBO_BOX(
+				gtk_grid_get_child_at(example_filter_grid, 1, i)));
+		if (combo_req) {
+			GtkWidget *selector = gtk_grid_get_child_at(example_filter_grid,
+					2, i);
+			example_requirements[j].attr = attrs[i].attr;
+			example_requirements[j].req = combo_req - 1;
+			if (attrs[i].type == ATTR_INT || attrs[i].type == ATTR_UNSIGNED) {
+				example_requirements[j].value
+						= gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(
+							selector));
+			} else {
+				example_requirements[j].value = gtk_combo_box_get_active(
+						GTK_COMBO_BOX(selector));
+			}
+			++j;
+		}
+	}
+}
--- src/example/main.c
+++ src/example/main.c
@@ -29,12 +29,17 @@
 static GtkWidget *window;
 static GtkListStore *visual_list_store;
 static GtkDialog *chooser;
+GtkDialog *example_filter_dialog;
+GtkGrid *example_filter_grid;
 static GtkGLCanvas *canvas;
 static GtkLabel *context_info_label, *mouse_info_label;
 static GtkTreeSelection *visual_selection;
 static GtkAdjustment *major_adjust, *minor_adjust;
 static GtkComboBox *profile_combo;
 static GtkButton *create_button, *destroy_button, *start_button, *stop_button;
+
+GtkGLRequirement *example_requirements;
+GtkGLVisualList *example_visuals, *example_choice;
 
 // Whether the animation is running
 static gboolean running = TRUE;
@@ -418,52 +423,64 @@ update_context_info(void) {
 }
 
 
+static void
+update_visual_list(void) {
+	// Initialize the context chooser dialog with existing visuals
+	static const GtkGLRequirement no_requirements[] = { GTK_GL_LIST_END };
+	size_t i;
+
+	if (example_choice) gtk_gl_visual_list_free(example_choice);
+	example_choice = gtk_gl_choose_visuals(example_visuals,
+			example_requirements ? example_requirements : no_requirements);
+
+	gtk_list_store_clear(visual_list_store);
+	for (i = 0; i < example_choice->count; ++i) {
+		GtkGLFramebufferConfig cfg;
+		gtk_gl_describe_visual(example_choice->entries[i], &cfg);
+		gtk_list_store_insert_with_values(visual_list_store, NULL, -1,
+			0, cfg.accelerated,
+			1, cfg.color_types & GTK_GL_COLOR_RGBA ? "RGBA" : "Indexed",
+			2, cfg.color_bpp,
+			3, cfg.fb_level,
+			4, cfg.double_buffered,
+			5, cfg.stereo_buffered,
+			6, cfg.aux_buffers,
+			7, cfg.red_color_bpp,
+			8, cfg.green_color_bpp,
+			9, cfg.blue_color_bpp,
+			10, cfg.alpha_color_bpp,
+			11, cfg.depth_bpp,
+			12, cfg.stencil_bpp,
+			13, cfg.red_accum_bpp,
+			14, cfg.green_accum_bpp,
+			15, cfg.blue_accum_bpp,
+			16, cfg.alpha_accum_bpp,
+			17, cfg.transparent_type == GTK_GL_TRANSPARENT_NONE ? "None"
+				: cfg.transparent_type == GTK_GL_TRANSPARENT_RGB ? "RGB"
+				: "Indexed",
+			18, cfg.sample_buffers,
+			19, cfg.samples_per_pixel,
+			20, cfg.caveat == GTK_GL_CAVEAT_SLOW ? "Slow"
+				: cfg.caveat == GTK_GL_CAVEAT_NONCONFORMANT
+					? "Non-conformant": "-",
+			-1);
+	}
+}
+
+
 // Handler for the "Create context..." button
 gboolean
 example_create_context(void) {
 	if (gtk_gl_canvas_has_context(canvas)) {
 		message_box(GTK_MESSAGE_ERROR, "Context exists already");
     } else {
-		// Initialize the context chooser dialog with existing visuals
-
-		GtkGLVisualList *visuals = gtk_gl_canvas_enumerate_visuals(canvas);
-		size_t i;
 		GtkTreeIter iter;
 		unsigned ver_major, ver_minor;
 		GtkGLProfile profile;
+		size_t i;
 
-		gtk_list_store_clear(visual_list_store);
-		for (i = 0; i < visuals->count; ++i) {
-			GtkGLFramebufferConfig cfg;
-			gtk_gl_describe_visual(visuals->entries[i], &cfg);
-			gtk_list_store_insert_with_values(visual_list_store, NULL, -1,
-				0, cfg.accelerated,
-				1, cfg.color_types & GTK_GL_COLOR_RGBA ? "RGBA" : "Indexed",
-				2, cfg.color_bpp,
-				3, cfg.fb_level,
-				4, cfg.double_buffered,
-				5, cfg.stereo_buffered,
-				6, cfg.aux_buffers,
-				7, cfg.red_color_bpp,
-				8, cfg.green_color_bpp,
-				9, cfg.blue_color_bpp,
-				10, cfg.alpha_color_bpp,
-				11, cfg.depth_bpp,
-				12, cfg.stencil_bpp,
-				13, cfg.red_accum_bpp,
-				14, cfg.green_accum_bpp,
-				15, cfg.blue_accum_bpp,
-				16, cfg.alpha_accum_bpp,
-				17, cfg.transparent_type == GTK_GL_TRANSPARENT_NONE ? "None"
-					: cfg.transparent_type == GTK_GL_TRANSPARENT_RGB ? "RGB"
-					: "Indexed",
-				18, cfg.sample_buffers,
-				19, cfg.samples_per_pixel,
-				20, cfg.caveat == GTK_GL_CAVEAT_SLOW ? "Slow"
-					: cfg.caveat == GTK_GL_CAVEAT_NONCONFORMANT
-						? "Non-conformant": "-",
-				-1);
-		}
+		example_visuals = gtk_gl_canvas_enumerate_visuals(canvas);
+		update_visual_list();
 
 		// Show the chooser dialog
 		gtk_dialog_run(chooser);
@@ -489,13 +506,17 @@ example_create_context(void) {
 
 		// Try creating and initializing a context
 		if (gtk_gl_canvas_create_context_with_version(canvas,
-				visuals->entries[i], ver_major, ver_minor, profile)) {
+				example_choice->entries[i], ver_major, ver_minor, profile)) {
 			init_context();
 			example_draw();
 		} else {
 			message_box(GTK_MESSAGE_ERROR, "Error creating context");
 		}
-		gtk_gl_visual_list_free(visuals);
+
+		gtk_gl_visual_list_free(example_choice);
+		example_choice = NULL;
+		gtk_gl_visual_list_free(example_visuals);
+		example_visuals = NULL;
     }
 
 	update_context_info();
@@ -542,6 +563,18 @@ example_mouse_leave(GtkWidget *widget, GdkEventCrossing *ev) {
 }
 
 
+void example_init_filter_dialog(void);
+void example_filter_dialog_run(void);
+
+
+gboolean
+example_filter_visuals(GtkWidget *widget, GdkEvent *ev) {
+	example_filter_dialog_run();
+	update_visual_list();
+	return FALSE;
+}
+
+
 int
 main(int argc, char *argv[]) {
 	GtkBuilder *builder;
@@ -561,6 +594,8 @@ main(int argc, char *argv[]) {
 	context_info_label = GTK_LABEL(GET("context-info-label"));
 	mouse_info_label = GTK_LABEL(GET("mouse-info-label"));
 	chooser = GTK_DIALOG(GET("context-chooser"));
+	example_filter_dialog = GTK_DIALOG(GET("filter-dialog"));
+	example_filter_grid = GTK_GRID(GET("filter-grid"));
 	visual_list_store = GTK_LIST_STORE(GET("visual-list-store"));
 	canvas = GTK_GL_CANVAS(GET("canvas"));
 	visual_selection = gtk_tree_view_get_selection(
@@ -577,6 +612,8 @@ main(int argc, char *argv[]) {
 
 	gtk_tree_selection_set_mode(visual_selection, GTK_SELECTION_SINGLE);
 	update_context_info();
+
+	example_init_filter_dialog();
 
 	gtk_widget_show_all(window);
 	g_object_unref(builder);
